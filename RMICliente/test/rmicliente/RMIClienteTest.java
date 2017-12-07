03e3fe73d279f42619edef9a3f391cba8098884e
Jos <Jos@192.168.1.40>
2017-12-07 12:09:08 +0100

    Creacion Formulario Login

--- RMICliente/test/rmicliente/RMIClienteTest.java
+++ RMICliente/test/rmicliente/RMIClienteTest.java
@@ -44,7 +44,7 @@ public class RMIClienteTest {
     public void testMain() {
         System.out.println("main");
         String[] args = null;
-        rmicliente.main(args);
+        Formulario.main(args);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
